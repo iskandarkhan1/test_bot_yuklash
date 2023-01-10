2488a6a3634a20feeb8a7b361bfb03ff5d8cc9d9
nicecoder4144 <115159558+nicecoder4144@users.noreply.github.com>
2023-01-10 14:23:28 +0500

    UPDATE CONFIG.PY

--- data/config.py
+++ data/config.py
@@ -9,11 +9,7 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
 
-CHANNELS = [-1001867295789]
-
-
-
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+DB_USER="postgres"
+DB_PASS='YN9ShYD0fU9MGJMuXs9X' 
+DB_NAME='railway'
+DB_HOST='containers-us-west-170.railway.app'
